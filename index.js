29564cadaef6e38c2749808015e7d08ccf7993dc
Hemanathan H P <hphemanathan@gmail.com>
2024-11-12 01:11:07 +0530

    wip

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log ("test");
+
+
+const input = document.querySelector("input");
+const subscribe = document.querySelector(".hero__content__btn");
+
+function handleSubscribe () {
+    const emailId = input.value;
+    console.log (emailId);
+    if(!emailId) {
+        console.log ("enter email address");
+         }else if(!emailId.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
+            console.log("valid email required")
+}else {
+console.log("success");
+}}
+
+subscribe.addEventListener("click", handleSubscribe);
+
+
