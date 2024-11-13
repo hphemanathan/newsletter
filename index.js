ef654526aee08f7cd4a05d9238e96972caaa523d
Hemanathan H P <hphemanathan@gmail.com>
2024-11-14 01:28:48 +0530

    wip

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
 
 const input = document.querySelector("input");
 const subscribe = document.querySelector(".hero__content__btn");
+const emptyLabel = document.querySelector(".hero__content__label-empty");
+const errorLabel = document.querySelector(".hero__content__label-error");
 
 function handleSubscribe () {
     const emailId = input.value;
-    console.log (emailId);
     if(!emailId) {
-        console.log ("enter email address");
+    console.log(emptyLabel);
          }else if(!emailId.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
             console.log("valid email required")
+            console.log(errorLabel);
 }else {
 console.log("success");
 }}
