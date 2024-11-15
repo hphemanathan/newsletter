c1ecb4570ce468ccb2ae338fce6ea8e7e2da8d75
Hemanathan H P <hphemanathan@gmail.com>
2024-11-16 01:21:43 +0530

    wip

--- index.js
+++ index.js
@@ -4,16 +4,37 @@ const input = document.querySelector("input");
 const subscribe = document.querySelector(".hero__content__btn");
 const emptyLabel = document.querySelector(".hero__content__label-empty");
 const errorLabel = document.querySelector(".hero__content__label-error");
+const successPage = document.querySelector(".success");
+const container = document.querySelector(".container");
+
+
+function emailIdEmpty() {
+    emptyLabel.classList.add("active");
+    input.classList.add("active");
+      errorLabel.classList.remove("active");
+
+}
+
+function emailIDError() {
+emptyLabel.classList.remove("active");
+  errorLabel.classList.add("active");
+  input.classList.add("active");
+}
+
+function emailIdSuccess() {
+    successPage.classList.add("active");
+    container.classList.add("active");
+
+}
 
 function handleSubscribe () {
     const emailId = input.value;
     if(!emailId) {
-    console.log(emptyLabel);
+    emailIdEmpty ();
          }else if(!emailId.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
-            console.log("valid email required")
-            console.log(errorLabel);
+            emailIDError();
 }else {
-console.log("success");
+emailIdSuccess();
 }}
 
 subscribe.addEventListener("click", handleSubscribe);
